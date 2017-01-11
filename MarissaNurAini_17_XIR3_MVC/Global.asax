7a9e98fbf143a79ef723e014022e276c3b67af63
Marissa Nur Aini <marissarissarissa19@gmail.com>
2017-01-11 12:31:16 +0700

    Add project files.

---
+++ MarissaNurAini_17_XIR3_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MarissaNurAini_17_XIR3_MVC.MvcApplication" Language="C#" %>
---
+++ MarissaNurAini_17_XIR3_MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MarissaNurAini_17_XIR3_MVC.Startup))]
+namespace MarissaNurAini_17_XIR3_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
